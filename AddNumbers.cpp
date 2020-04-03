f61f401e2eef45e04634359b02b6e424ea597e3e
v4life <veer.shresti@gmail.com>
2020-04-02 21:52:18 -0700

    Adding HelloUniverse

---
+++ AddNumbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int sum = 0;
+    for (int i = 0; i < 11; i++)
+    {
+        sum += i;
+    }
+    cout << "Sum = " << sum << endl;
+
+    return 0;
+}
---
+++ HelloUniverse.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+//
+// Created by Veer Shresti on 4/2/20.
+//
+
+using namespace std;
+
+int main()
+{
+    cout<<"Hello Universe"<<endl;
+}
